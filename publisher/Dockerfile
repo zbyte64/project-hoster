4421b6c2d116d6f2b8b2fd2eebdff2b227a54b1a
Jason Kraus <zbyte64@gmail.com>
2016-09-28 15:40:34 -0700

    make express application importable

--- publisher/Dockerfile
+++ publisher/Dockerfile
@@ -7,4 +7,4 @@ RUN npm install
 
 
 EXPOSE 8000
-CMD ["node", "src/index.js"]
+CMD ["npm", "start"]
--- publisher/src/index.js
+++ publisher/src/index.js
@@ -1,12 +1,12 @@
-/*
-var appPath = require('app-module-path');
-appPath.addPath(__dirname+'/../submodules');
-*/
 var {app} = require('./app');
 
-var server = app.listen(8000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+exports.app = app
 
-  console.log(`Publisher is listening at http://${host}:${port} hosterUrl: ${process.env.HOSTER_RPC_URL}`)
-});
+if (require.main === module) {
+  var server = app.listen(8000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log(`Publisher is listening at http://${host}:${port} hosterUrl: ${process.env.HOSTER_RPC_URL}`)
+  });
+}
